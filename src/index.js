8c7f4d1087b51c3ed80200b1249472cf42fe2771
dnavault <47720676+dnavault@users.noreply.github.com>
2021-08-27 15:34:20 -0400

    Initial

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ var db = new sqlite3.Database(":memory");
 //create a server object:
 http
   .createServer(function (req, res) {
+    db.serialize(function () {
+      db.run("CREATE TABLE products (info TEXT)");
+
+      var stmt = db.prepare("INSERT INTO products VALUES (?)");
+      for (var i = 0; i < 10; i++) {
+        stmt.run("Product name " + i);
+      }
+      stmt.finalize();
+
+      db.each("SELECT rowid AS id, info FROM products", function (err, row) {
+        console.log(row.id + ": " + row.info);
+      });
+    });
+    db.close();
+
     res.write("Hello dded!"); //write a response to the client
     res.end(); //end the response
   })
